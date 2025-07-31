297b7643da069daf3e554c7df9ff728d5a7b4696
josechezuria <devlab@josechezuria.com>
2025-07-31 11:06:56 -0500

    Add roll button,arr change to arr of objects,dice toggle green functionality

--- components/Die.jsx
+++ components/Die.jsx
@@ -1,11 +1,19 @@
-function Die (props) {
-    return (
-        <>
-            <button className="die">
-                {props.value}
-            </button>
-        </>
-    )
+import { useState } from "react";
+
+function Die(props) {
+  const [isActive, setIsActive] = useState(props.isHeld);
+
+  const toggleActive = () => {
+    setIsActive(!isActive);
+  };
+
+  return (
+    <>
+      <button className={isActive ? "active-button" : "die"} onClick={toggleActive}>
+        {props.value}
+      </button>
+    </>
+  );
 }
 
 export default Die;
--- src/App.jsx
+++ src/App.jsx
@@ -1,30 +1,32 @@
 import "./App.css";
 import Die from "../components/Die";
 import { useState } from "react";
-
+import { nanoid } from "nanoid";
 
 function App() {
+  const [dice, setDice] = useState(generatedAllNewDice());
 
-  const [dice, setDice] = useState(generatedAllNewDice())
-
+  function rollDice() {
+    setDice(generatedAllNewDice());
+  }
 
   function generatedAllNewDice() {
     const arr = [];
 
     for (let i = 0; i < 10; i++) {
       const randomNumber = Math.floor(Math.random() * 6) + 1;
-       arr.push(randomNumber);
+      arr.push({
+        value: randomNumber,
+        isHeld: true,
+        id: nanoid(),
+      });
     }
     return arr;
   }
 
-  console.log(generatedAllNewDice());
-
-  const generateDice = dice.map(die => (
-    <Die
-      value={die}
-    />
-  ))
+  const diceElements = dice.map((die) => (
+    <Die key={die.id} value={die.value} isHeld={die.isHeld} />
+  ));
 
   return (
     <main>
@@ -33,7 +35,10 @@ function App() {
         <div className="outer-square">
           <div className="inner-square">
             <div className="dice-container">
-                {generateDice}
+              {diceElements}
+              <button className="roll" onClick={rollDice}>
+                Roll
+              </button>
             </div>
           </div>
         </div>
